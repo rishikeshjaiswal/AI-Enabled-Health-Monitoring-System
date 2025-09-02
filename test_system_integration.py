d69d0571d8c3871908eb8b4471b3cca6d06b75d8
Rishikesh Jaiswal <jrishikesh125@gmail.com>
2025-09-02 15:36:38 +0530

    Create test_system_integration.py

---
+++ test_system_integration.py
@@ -0,0 +1,16 @@
+import unittest
+from health_monitoring.monitor import HealthMonitor
+
+class TestSystemIntegration(unittest.TestCase):
+    def setUp(self):
+        self.monitor = HealthMonitor()
+
+    def test_end_to_end(self):
+        """Simulate system integration test"""
+        data_stream = [
+            [75, 122, 98.7],
+            [110, 150, 101.2],
+            [65, 115, 97.5]
+        ]
+        results = [self.monitor.predict_health(d)[0] for d in data_stream]
+        self.assertEqual(len(results), 3)  # 3 predictions
