288e809705cb4e51eac3647d59bf433d7a88c69c
Rishikesh Jaiswal <jrishikesh125@gmail.com>
2025-09-02 15:35:53 +0530

    Create test_model_inputs.py

---
+++ test_model_inputs.py
@@ -0,0 +1,11 @@
+import unittest
+from health_monitoring.monitor import HealthMonitor
+
+class TestModelInputs(unittest.TestCase):
+    def setUp(self):
+        self.monitor = HealthMonitor()
+
+    def test_invalid_input(self):
+        """Model should raise error for wrong input shape"""
+        with self.assertRaises(Exception):
+            self.monitor.predict_health([999])  # invalid input
