d133df6f8bf1b21c7333fe057b25ea9ca0a6f568
Rishikesh Jaiswal <jrishikesh125@gmail.com>
2025-09-02 15:33:49 +0530

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,15 @@
+import pickle
+import numpy as np
+
+class HealthMonitor:
+    def __init__(self, model_path="models/health_model.pkl"):
+        with open(model_path, "rb") as f:
+            self.model = pickle.load(f)
+
+    def predict_health(self, data):
+        """
+        Predicts health status from input data.
+        data: numpy array [heart_rate, blood_pressure, temperature]
+        """
+        arr = np.array([data])
+        return self.model.predict(arr)
